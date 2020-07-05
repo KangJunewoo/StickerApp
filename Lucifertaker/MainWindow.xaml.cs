11c1db130ddf2fc0f9692bb29aa920d119893baa
KangJunewoo <brianjune@naver.com>
2020-07-05 21:25:29 +0900

    기본기능 구현 성공

--- Lucifertaker/MainWindow.xaml.cs
+++ Lucifertaker/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Configuration;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +11,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Lucifertaker
 {
@@ -20,9 +25,55 @@ namespace Lucifertaker
     /// </summary>
     public partial class MainWindow : Window
     {
+        Bitmap original;
+        Bitmap[] frames = new Bitmap[12];
+        ImageSource[] imgFrame = new ImageSource[12];
+        string bitmapPath = "Resources/Lucifer.png";
+        int frame = -1;
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        // for releasing bitmap
         public MainWindow()
         {
             InitializeComponent();
+
+            original = System.Drawing.Image.FromFile(bitmapPath) as Bitmap;
+            for(int i = 0; i < 12; i++)
+            {
+                frames[i] = new Bitmap(100, 100);
+                using(Graphics g= Graphics.FromImage(frames[i]))
+                {
+                    g.DrawImage(original, new System.Drawing.Rectangle(0, 0, 100, 100),
+                        new System.Drawing.Rectangle(i * 100, 0, 100, 100),
+                        GraphicsUnit.Pixel);
+                }
+                var handle = frames[i].GetHbitmap();
+                try
+                {
+                    imgFrame[i] = Imaging.CreateBitmapSourceFromHBitmap(handle,
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+                finally
+                {
+                    // dll에서 불러온건가..? 범상치 않다.
+                    DeleteObject(handle);
+                }
+            }
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.0167*3);
+            timer.Tick += NextFrame;
+            timer.Start();
+        }
+
+        private void NextFrame(object sender, EventArgs e)
+        {
+            frame = (frame + 1) % 12;
+            iLucifer.Source = imgFrame[frame];
         }
     }
 }
